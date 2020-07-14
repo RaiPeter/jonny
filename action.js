8e07373f29f4167abaac082b5558fad6231deef6
RaiPeter <rai.peter2@gmail.com>
2020-07-14 23:38:44 +0530

    responsive

--- action.js
+++ action.js
@@ -14,3 +14,17 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+// document.getElementById("myTopnav")
+
+function openNav() {
+  console.log("hello");
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+    console.log("hau");
+  } else {
+    x.className = "topnav";
+  }
+}
